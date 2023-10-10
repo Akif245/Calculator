7981525f2068044f27a984f480c998a1069145bf
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-10-11 01:07:47 +0530

    Updated Version

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ let string = "";
 let buttons = document.querySelectorAll('.button')
 Array.from(buttons).forEach((button )=>{
           button .addEventListener('click',(e)=>{
-                    console.log(e.target);
+                    if(e.target.innerHTML == 'ANS'){
+                              string =eval(string);
+                              document.querySelector('input').value = string
+                    }
+                    else if(e.target.innerHTML == 'C'){
+                              string = ""
+                              document.querySelector('input').value = string
+                    } 
+                   
+                    
+                    else{
+                              console.log(e.target);
+                              string = string + e.target.innerHTML;
+                              document.querySelector('input').value =string
+                    }
 
           })
 })
